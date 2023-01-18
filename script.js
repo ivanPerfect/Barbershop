640b64e1e609cd97aec752f9867704951282dc1a
Ivan <mad.genius69@gmail.com>
2023-01-18 09:57:30 +0200

    18.01.23

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelectorAll('.btn2, .btn_barber_1, .btn_barber_2, .btn_barber_3, .btn_barber_4');
+const button = document.querySelectorAll('.btn2, .btn_barber_1, .btn_barber_2, .btn_barber_3, .btn_barber_4, .btn1');
 let buttonLenth = button.length;
 const form = document.querySelector('#blablabla');
 const popup = document.querySelector('.popup');
@@ -15,5 +15,15 @@ button1.addEventListener('click', () => {
   popup.classList.remove('popup_open');
 });
 
-
-
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) => {
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else{
+      entry.target.classList.remove('show');
+    }
+  })
+})
+const hiddenEl = document.querySelectorAll(".hidden");
+hiddenEl.forEach((element) => observer.observe(element));
