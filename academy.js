c0a266f859aff4064cb2b2851e80e1d44a357fb9
Ivan <mad.genius69@gmail.com>
2023-01-20 10:55:50 +0200

    20.01.2023

--- academy.js
+++ academy.js
@@ -3,10 +3,24 @@ const observer = new IntersectionObserver((entries)=>{
       if (entry.isIntersecting){
         entry.target.classList.add('show');
       }
-      else{
-        entry.target.classList.remove('show');
-      }
     })
   })
   const hiddenEl = document.querySelectorAll(".hidden");
   hiddenEl.forEach((element) => observer.observe(element));
+
+
+  const btn1 = document.querySelector('.btn1');
+  const popup = document.querySelector('.popup');
+  const form = document.querySelector('#blablabla');
+  const buttonClose = document.querySelector('.button_close');
+
+
+  btn1.addEventListener('click', ()=>{
+    form.classList.add('open');
+    popup.classList.add('popup_open');
+  });
+
+  buttonClose.addEventListener('click', ()=> {
+    form.classList.remove('open');
+    popup.classList.remove('popup_open');
+  })
--- script.js
+++ script.js
@@ -2,26 +2,27 @@ const button = document.querySelectorAll('.btn2, .btn_barber_1, .btn_barber_2, .
 let buttonLenth = button.length;
 const form = document.querySelector('#blablabla');
 const popup = document.querySelector('.popup');
-const button1 = document.querySelector('.btn4');
+const button1 = document.querySelectorAll('.btn4 , .button_close');
+let buttonLenth2= button1.length;
 
 for (let i =0; i < buttonLenth; i++){
   button[i].addEventListener('click', () => {
     form.classList.add('open');
     popup.classList.add('popup_open');
   });
-}
-button1.addEventListener('click', () => {
-  form.classList.remove('open');
-  popup.classList.remove('popup_open');
+};
+
+for (let index = 0; index<buttonLenth2; index++){
+  button1[index].addEventListener('click', ()=> {
+    form.classList.remove('open');
+    popup.classList.remove('popup_open');
 });
+};
 
 const observer = new IntersectionObserver((entries)=>{
   entries.forEach((entry) => {
     if (entry.isIntersecting){
       entry.target.classList.add('show');
-    }
-    else{
-      entry.target.classList.remove('show');
     }
   })
 })
